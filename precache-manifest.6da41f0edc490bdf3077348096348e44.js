5bc81cbbff288280b75d8b3d2c06a07bec1c6b16
Đình Trần Phương <dinhblade123@gmail.com>
2021-05-03 18:56:51 +0700

    Updates

---
+++ precache-manifest.6da41f0edc490bdf3077348096348e44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a211fb4a6f1011934dab6c4cf4a055",
+    "url": "/MemeGenerator/index.html"
+  },
+  {
+    "revision": "0e046dffe5047ed8aa0d",
+    "url": "/MemeGenerator/static/css/main.05f13152.chunk.css"
+  },
+  {
+    "revision": "062ea168f5659fb91666",
+    "url": "/MemeGenerator/static/js/2.da423be0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MemeGenerator/static/js/2.da423be0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e046dffe5047ed8aa0d",
+    "url": "/MemeGenerator/static/js/main.ee3056e7.chunk.js"
+  },
+  {
+    "revision": "dfe8061885248b7eace3",
+    "url": "/MemeGenerator/static/js/runtime-main.8b63568b.js"
+  }
+]);
